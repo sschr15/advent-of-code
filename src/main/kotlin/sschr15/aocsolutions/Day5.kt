0d6ef9cdc801df6a1a07cb981b564c6e4bbe4899
sschr15 <steven.sschr15@hotmail.com>
2021-12-05 22:30:22 -0600

    add timing

--- src/main/kotlin/sschr15/aocsolutions/Day5.kt
+++ src/main/kotlin/sschr15/aocsolutions/Day5.kt
@@ -1,6 +1,7 @@
 package sschr15.aocsolutions
 
 import kotlin.math.*
+import kotlin.system.measureTimeMillis
 
 /**
  * Day 5: Hydrothermal Venture
@@ -13,25 +14,45 @@ fun day5() {
 
     val grid = Grid(1000, 1000, 0)
 
-    val lines = input.map { it.split(" -> ") }.map { (p1, p2) ->
-        val (x1, y1) = p1.split(",").map { it.toInt() }
-        val (x2, y2) = p2.split(",").map { it.toInt() }
+    val lines = mutableListOf<Line>()
 
-        Line(Point(x1, y1), Point(x2, y2))
+    measureTimeMillis {
+        lines.addAll(input.map { it.split(" -> ") }.map { (p1, p2) ->
+            val (x1, y1) = p1.split(",").map { it.toInt() }
+            val (x2, y2) = p2.split(",").map { it.toInt() }
+
+            Line(Point(x1, y1), Point(x2, y2))
+        })
+    }.also {
+        if (System.getProperty("debug") != null) {
+            println("Time to parse input: $it ms")
+        }
     }
 
     // Part 1
-    lines.filter { it.slope == 0.0 || it.slope.isInfinite() }
-        .flatMap { it.wholeNumberPoints }
-        .filter { (x, y) -> x >= 0 && y >= 0 && x < grid.width && y < grid.height }
-        .forEach { grid[it] = grid[it] + 1 }
+    measureTimeMillis {
+        lines.filter { it.slope == 0.0 || it.slope.isInfinite() }
+            .flatMap { it.wholeNumberPoints }
+            .filter { (x, y) -> x >= 0 && y >= 0 && x < grid.width && y < grid.height }
+            .forEach { grid[it] = grid[it] + 1 }
+    }.also {
+        if (System.getProperty("debug") != null) {
+            println("Time to calculate intersections (Part 1): $it ms")
+        }
+    }
 
     println("Part 1: ${grid.flatten().count { it > 1 }}")
 
     // Part 2
-    lines.filter { it.slope.absoluteValue == 1.0 }.flatMap { it.wholeNumberPoints }
-        .filter { (x, y) -> x >= 0 && y >= 0 && x < grid.width && y < grid.height }
-        .forEach { grid[it] = grid[it] + 1 }
+    measureTimeMillis {
+        lines.filter { it.slope.absoluteValue == 1.0 }.flatMap { it.wholeNumberPoints }
+            .filter { (x, y) -> x >= 0 && y >= 0 && x < grid.width && y < grid.height }
+            .forEach { grid[it] = grid[it] + 1 }
+    }.also {
+        if (System.getProperty("debug") != null) {
+            println("Time to calculate intersections (Part 2): $it ms")
+        }
+    }
 
     println("Part 2: ${grid.flatten().count { it > 1 }}")
 }
--- src/main/kotlin/sschr15/aocsolutions/Main.kt
+++ src/main/kotlin/sschr15/aocsolutions/Main.kt
@@ -4,41 +4,56 @@ import java.io.PrintWriter
 import kotlin.io.path.Path
 import kotlin.io.path.bufferedWriter
 import kotlin.system.measureTimeMillis
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 private var anythingFailed = false
 
+@OptIn(ExperimentalTime::class)
 fun main(args: Array<String>) {
     // in case there are slow solves, we can skip them if necessary
     val shouldExecuteSlowFunctions = args.contains("--include-slow")
 
-    runCatching {
-        println("Day 1: Sonar Sweep")
-        println("Completed in ${measureTimeMillis { day1() }}ms")
-    }.onFailure { failure(it, 1) }
+    println("Total time: ${
+        measureTime {
+            runCatching {
+                println("Day 1: Sonar Sweep")
+                println("Completed in ${measureTimeMillis { day1() }}ms")
+            }.onFailure { failure(it, 1) }
 
-    runCatching {
-        println("Day 2: Dive!")
-        println("Completed in ${measureTimeMillis { day2() }}ms")
-    }.onFailure { failure(it, 2) }
+            runCatching {
+                println("Day 2: Dive!")
+                println("Completed in ${measureTimeMillis { day2() }}ms")
+            }.onFailure { failure(it, 2) }
 
-    runCatching {
-        println("Day 3: Binary Diagnostic")
-        println("Completed in ${measureTimeMillis { day3() }}ms")
-    }.onFailure { failure(it, 3) }
+            runCatching {
+                println("Day 3: Binary Diagnostic")
+                println("Completed in ${measureTimeMillis { day3() }}ms")
+            }.onFailure { failure(it, 3) }
 
-    runCatching {
-        println("Day 4: Giant Squid")
-        println("Completed in ${measureTimeMillis { day4() }}ms")
-    }.onFailure { failure(it, 4) }
+            runCatching {
+                println("Day 4: Giant Squid")
+                println("Completed in ${measureTimeMillis { day4() }}ms")
+            }.onFailure { failure(it, 4) }
 
-    runCatching {
-        println("Day 5: Hydrothermal Venture")
-        println("Completed in ${measureTimeMillis { day5() }}ms")
-    }.onFailure { failure(it, 5) }
+            runCatching {
+                println("Day 5: Hydrothermal Venture")
+                println("Completed in ${measureTimeMillis { day5() }}ms")
+            }.onFailure { failure(it, 5) }
 
-    if (anythingFailed) {
-        println("Stacktraces for failed solutions are in files under the name `dayN_error.txt`")
-    }
+            if (anythingFailed) {
+                println("Stacktraces for failed solutions are in files under the name `dayN_error.txt`")
+            }
+        }.let { 
+            val mins = it.inWholeMinutes.toString().padStart(2, '0')
+            val secs = it.toDouble(DurationUnit.SECONDS).toString().let { s ->
+                s.split(".")[0].padStart(2, '0') + "." + s.split(".")[1]
+            }
+            
+            "$mins:$secs"
+        }
+    }")
 }
 
 private fun failure(t: Throwable, day: Int) {
