7e01ff0b64a5675d5594f6951f05a7c70c4c6a8a
sschr15 <steven.sschr15@hotmail.com>
2021-12-07 09:45:59 -0600

    better kotlin duration stufs

--- src/main/kotlin/sschr15/aocsolutions/Main.kt
+++ src/main/kotlin/sschr15/aocsolutions/Main.kt
@@ -5,7 +5,6 @@ import java.io.PrintWriter
 import kotlin.io.path.Path
 import kotlin.io.path.bufferedWriter
 import kotlin.system.measureTimeMillis
-import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
@@ -18,68 +17,61 @@ fun main(args: Array<String>) {
 
     println("Total time: ${
         measureTime {
-            val originalOut = System.out
-            System.setOut(ThreadLocalPrintStream())
-            
-            runCatching(1) {
-                println("Day 1: Sonar Sweep")
-                println("Completed in ${measureTimeMillis { day1() }}ms")
-            }
-
-            runCatching(2) {
-                println("Day 2: Dive!")
-                println("Completed in ${measureTimeMillis { day2() }}ms")
-            }
-
-            runCatching(3) {
-                println("Day 3: Binary Diagnostic")
-                println("Completed in ${measureTimeMillis { day3() }}ms")
-            }
-
-            runCatching(4) {
-                println("Day 4: Giant Squid")
-                println("Completed in ${measureTimeMillis { day4() }}ms")
-            }
-
-            runCatching(5) {
-                println("Day 5: Hydrothermal Venture")
-                println("Completed in ${measureTimeMillis { day5() }}ms")
-            }
-            
-            runCatching(6) {
-                println("Day 6: Lanternfish")
-                println("Completed in ${measureTimeMillis { day6() }}ms")
-            }
-            
-            runCatching(7) {
-                println("Day 7: The Treachery of Whales")
-                println("Completed in ${measureTimeMillis { day7() }}ms")
-            }
-
-            // wait for all threads (in the `threads` list) to finish
-            while (threads.any { it.isAlive }) {
-                Thread.sleep(100)
-            }
-
-            // restore the original output stream
-            System.setOut(originalOut)
-
-            // combine all ByteArrays in `output` into the print stream
-            outputs.toList().sortedBy { it.first }.forEach { (_, output) ->
-                println(output.toString(Charsets.UTF_8))
-            }
-
-            if (anythingFailed) {
-                println("Stacktraces for failed solutions are in files under the name `dayN_error.txt`")
-            }
-        }.let { 
-            val mins = it.inWholeMinutes.toString().padStart(2, '0')
-            val secs = it.toDouble(DurationUnit.SECONDS).toString().let { s ->
-                s.split(".")[0].padStart(2, '0') + "." + s.split(".")[1]
-            }
-            
-            "$mins:$secs"
+        val originalOut = System.out
+        System.setOut(ThreadLocalPrintStream())
+        
+        runCatching(1) {
+            println("Day 1: Sonar Sweep")
+            println("Completed in ${measureTimeMillis { day1() }}ms")
         }
+
+        runCatching(2) {
+            println("Day 2: Dive!")
+            println("Completed in ${measureTimeMillis { day2() }}ms")
+        }
+
+        runCatching(3) {
+            println("Day 3: Binary Diagnostic")
+            println("Completed in ${measureTimeMillis { day3() }}ms")
+        }
+
+        runCatching(4) {
+            println("Day 4: Giant Squid")
+            println("Completed in ${measureTimeMillis { day4() }}ms")
+        }
+
+        runCatching(5) {
+            println("Day 5: Hydrothermal Venture")
+            println("Completed in ${measureTimeMillis { day5() }}ms")
+        }
+        
+        runCatching(6) {
+            println("Day 6: Lanternfish")
+            println("Completed in ${measureTimeMillis { day6() }}ms")
+        }
+        
+        runCatching(7) {
+            println("Day 7: The Treachery of Whales")
+            println("Completed in ${measureTimeMillis { day7() }}ms")
+        }
+
+        // wait for all threads (in the `threads` list) to finish
+        while (threads.any { it.isAlive }) {
+            Thread.sleep(100)
+        }
+
+        // restore the original output stream
+        System.setOut(originalOut)
+
+        // combine all ByteArrays in `output` into the print stream
+        outputs.toList().sortedBy { it.first }.forEach { (_, output) ->
+            println(output.toString(Charsets.UTF_8))
+        }
+
+        if (anythingFailed) {
+            println("Stacktraces for failed solutions are in files under the name `dayN_error.txt`")
+        }
+    }
     }")
 }
 
