1945488443154deb3924f798bde3753b9bc628af
sschr15 <steven.sschr15@hotmail.com>
2021-12-07 10:34:15 -0600

    A speedier solution found by @LlamaLad7

--- src/main/kotlin/sschr15/aocsolutions/Day7.kt
+++ src/main/kotlin/sschr15/aocsolutions/Day7.kt
@@ -8,24 +8,27 @@ import kotlin.math.min
 /**
  * Day 7: The Treachery of Whales
  *
- * Part 1: find the least fuel-expensive solution to move a list of ints to a single int
- * Part 2: each step takes 1 more fuel than the previous step
+ * - Part 1: find the least fuel-expensive solution to move a list of ints to a single int
+ * - Part 2: each step takes 1 more fuel than the previous step
  */
 fun day7() {
     val input = getChallenge(2021, 7, ",").map { it.removeSuffix("\n").toInt() }
 
-    val possibilities = input.minOrNull()!!..input.maxOrNull()!!
-    val result = possibilities.minOf {
-        input.sumOf { i -> (i - it).absoluteValue }
-    }
-    println("Part 1: $result")
+    // Part 1 has the ideal position being the median of the input values
+    val median = input.sorted().let { if (it.size % 2 == 0) (it[it.size / 2 - 1] + it[it.size / 2]) / 2 else it[it.size / 2] }
+    val part1 = input.sumOf { (it - median).absoluteValue }
 
-    // there's a geometric way of solving this ono
-    val result2 = possibilities.minOf {
+    println("Part 1: $part1")
+
+    // Part 2 has an ideal solution being the mean, +/- 1
+    val possiblePart2Solutions = input.average().let {
+        listOf(it.toInt(), it.toInt() + 1)
+    }
+    val part2 = possiblePart2Solutions.minOf {
         input.sumOf { i -> i.step(it) }
     }
 
-    println("Part 2: $result2")
+    println("Part 2: $part2")
 }
 
 fun main() {
@@ -35,5 +38,5 @@ fun main() {
 private fun Int.step(result: Int): Int {
     val start = min(this, result)
     val end = max(this, result)
-    return (start..end).sumOf { it - start }
+    return (0..(end - start)).sum()
 }
