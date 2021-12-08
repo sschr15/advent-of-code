94c94aa786df6ad14b3c8d0d24d7d29796dfea10
sschr15 <steven.sschr15@hotmail.com>
2021-12-08 17:36:29 -0600

    remembering to update main is hard

--- src/main/kotlin/sschr15/aocsolutions/Main.kt
+++ src/main/kotlin/sschr15/aocsolutions/Main.kt
@@ -17,61 +17,66 @@ fun main(args: Array<String>) {
 
     println("Total time: ${
         measureTime {
-        val originalOut = System.out
-        System.setOut(ThreadLocalPrintStream())
-        
-        runCatching(1) {
-            println("Day 1: Sonar Sweep")
-            println("Completed in ${measureTimeMillis { day1() }}ms")
-        }
+            val originalOut = System.out
+            System.setOut(ThreadLocalPrintStream())
 
-        runCatching(2) {
-            println("Day 2: Dive!")
-            println("Completed in ${measureTimeMillis { day2() }}ms")
-        }
+            runCatching(1) {
+                println("Day 1: Sonar Sweep")
+                println("Completed in ${measureTimeMillis { day1() }}ms")
+            }
 
-        runCatching(3) {
-            println("Day 3: Binary Diagnostic")
-            println("Completed in ${measureTimeMillis { day3() }}ms")
-        }
+            runCatching(2) {
+                println("Day 2: Dive!")
+                println("Completed in ${measureTimeMillis { day2() }}ms")
+            }
 
-        runCatching(4) {
-            println("Day 4: Giant Squid")
-            println("Completed in ${measureTimeMillis { day4() }}ms")
-        }
+            runCatching(3) {
+                println("Day 3: Binary Diagnostic")
+                println("Completed in ${measureTimeMillis { day3() }}ms")
+            }
 
-        runCatching(5) {
-            println("Day 5: Hydrothermal Venture")
-            println("Completed in ${measureTimeMillis { day5() }}ms")
-        }
-        
-        runCatching(6) {
-            println("Day 6: Lanternfish")
-            println("Completed in ${measureTimeMillis { day6() }}ms")
-        }
-        
-        runCatching(7) {
-            println("Day 7: The Treachery of Whales")
-            println("Completed in ${measureTimeMillis { day7() }}ms")
-        }
+            runCatching(4) {
+                println("Day 4: Giant Squid")
+                println("Completed in ${measureTimeMillis { day4() }}ms")
+            }
 
-        // wait for all threads (in the `threads` list) to finish
-        while (threads.any { it.isAlive }) {
-            Thread.sleep(100)
-        }
+            runCatching(5) {
+                println("Day 5: Hydrothermal Venture")
+                println("Completed in ${measureTimeMillis { day5() }}ms")
+            }
 
-        // restore the original output stream
-        System.setOut(originalOut)
+            runCatching(6) {
+                println("Day 6: Lanternfish")
+                println("Completed in ${measureTimeMillis { day6() }}ms")
+            }
 
-        // combine all ByteArrays in `output` into the print stream
-        outputs.toList().sortedBy { it.first }.forEach { (_, output) ->
-            println(output.toString(Charsets.UTF_8))
-        }
+            runCatching(7) {
+                println("Day 7: The Treachery of Whales")
+                println("Completed in ${measureTimeMillis { day7() }}ms")
+            }
 
-        if (anythingFailed) {
-            println("Stacktraces for failed solutions are in files under the name `dayN_error.txt`")
+            runCatching(8) {
+                println("Day 8: Seven Segment Search")
+                println("Completed in ${measureTimeMillis { day8() }}ms")
+            }
+
+            // wait for all threads (in the `threads` list) to finish
+            while (threads.any { it.isAlive }) {
+                Thread.sleep(100)
+            }
+
+            // restore the original output stream
+            System.setOut(originalOut)
+
+            // combine all ByteArrays in `output` into the print stream
+            outputs.toList().sortedBy { it.first }.forEach { (_, output) ->
+                println(output.toString(Charsets.UTF_8))
+            }
+
+            if (anythingFailed) {
+                println("Stacktraces for failed solutions are in files under the name `dayN_error.txt`")
+            }
         }
-    }
     }")
 }
 
