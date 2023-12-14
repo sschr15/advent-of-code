a861d24c0ee976c73fe05740213dade151b0076b
sschr15 <steven.sschr15@hotmail.com>
2023-12-14 16:18:10 -0600

    Change the name of the template class to make copypasta nicer

--- src/main/kotlin/sschr15/aocsolutions/_Template.kt
+++ src/main/kotlin/sschr15/aocsolutions/_Template.kt
@@ -8,7 +8,7 @@ import sschr15.aocsolutions.util.challenge
  * AOC 2023 [Day N](https://adventofcode.com/2023/day/N)
  * Challenge: TODO (based on the day's description)
  */
-object DayN : Challenge {
+object _Template : Challenge {
     @ReflectivelyUsed
     override fun solve() = challenge(2023, 0) {
 //        test()
