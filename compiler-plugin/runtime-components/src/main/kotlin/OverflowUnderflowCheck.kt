e1ec8bf9623dd2c8e90bdcc24f1262d456522371
sschr15 <steven.sschr15@hotmail.com>
2024-12-15 18:46:01 -0600

    Update secret names + overflow/underflow checks

--- compiler-plugin/runtime-components/src/main/kotlin/OverflowUnderflowCheck.kt
+++ compiler-plugin/runtime-components/src/main/kotlin/OverflowUnderflowCheck.kt
@@ -1,89 +1,17 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package sschr15.aoc.annotations
 
-fun plus(a: Int, b: Int): Int {
-    if (b < 0) return minus(a, -b)
-    if (a > Int.MAX_VALUE - b) {
-        throw ArithmeticException("Integer Overflow")
-    } else if (a < Int.MIN_VALUE + b) {
-        throw ArithmeticException("Integer Underflow")
-    }
-    return a + b
-}
+inline fun plus(a: Int, b: Int): Int = Math.addExact(a, b) 
+inline fun minus(a: Int, b: Int): Int = Math.subtractExact(a, b) 
+inline fun times(a: Int, b: Int): Int = Math.multiplyExact(a, b) 
+inline fun inc(a: Int): Int = Math.incrementExact(a) 
+inline fun dec(a: Int): Int = Math.decrementExact(a)
+inline fun unaryMinus(a: Int): Int = Math.negateExact(a)
 
-fun minus(a: Int, b: Int): Int {
-    if (b < 0) return plus(a, -b)
-    if (a < Int.MIN_VALUE + b) {
-        throw ArithmeticException("Integer Underflow")
-    } else if (a > Int.MAX_VALUE - b) {
-        throw ArithmeticException("Integer Overflow")
-    }
-    return a - b
-}
-
-fun times(a: Int, b: Int): Int {
-    if (b == 0) return 0
-    if (a > Int.MAX_VALUE / b) {
-        throw ArithmeticException("Integer Overflow")
-    } else if (a < Int.MIN_VALUE / b) {
-        throw ArithmeticException("Integer Underflow")
-    }
-    return a * b
-}
-
-fun inc(a: Int): Int {
-    if (a == Int.MAX_VALUE) {
-        throw ArithmeticException("Integer Overflow")
-    }
-    return a + 1
-}
-
-fun dec(a: Int): Int {
-    if (a == Int.MIN_VALUE) {
-        throw ArithmeticException("Integer Underflow")
-    }
-    return a - 1
-}
-
-fun plus(a: Long, b: Long): Long {
-    if (b < 0) return minus(a, -b)
-    if (a > Long.MAX_VALUE - b) {
-        throw ArithmeticException("Long Overflow")
-    } else if (a < Long.MIN_VALUE + b) {
-        throw ArithmeticException("Long Underflow")
-    }
-    return a + b
-}
-
-fun minus(a: Long, b: Long): Long {
-    if (b < 0) return plus(a, -b)
-    if (a < Long.MIN_VALUE + b) {
-        throw ArithmeticException("Long Underflow")
-    } else if (a > Long.MAX_VALUE - b) {
-        throw ArithmeticException("Long Overflow")
-    }
-    return a - b
-}
-
-fun times(a: Long, b: Long): Long {
-    if (b == 0L) return 0
-    if (a > Long.MAX_VALUE / b) {
-        throw ArithmeticException("Long Overflow")
-    } else if (a < Long.MIN_VALUE / b) {
-        throw ArithmeticException("Long Underflow")
-    }
-    return a * b
-}
-
-fun inc(a: Long): Long {
-    if (a == Long.MAX_VALUE) {
-        throw ArithmeticException("Long Overflow")
-    }
-    return a + 1
-}
-
-fun dec(a: Long): Long {
-    if (a == Long.MIN_VALUE) {
-        throw ArithmeticException("Long Underflow")
-    }
-    return a - 1
-}
+inline fun plus(a: Long, b: Long): Long = Math.addExact(a, b)
+inline fun minus(a: Long, b: Long): Long = Math.subtractExact(a, b)
+inline fun times(a: Long, b: Long): Long = Math.multiplyExact(a, b)
+inline fun inc(a: Long): Long = Math.incrementExact(a)
+inline fun dec(a: Long): Long = Math.decrementExact(a)
+inline fun unaryMinus(a: Long): Long = Math.negateExact(a)
