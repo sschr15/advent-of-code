f8aeff46c85cd02743d762a769214bb791f60366
sschr15 <steven.sschr15@hotmail.com>
2024-12-18 22:03:54 -0600

    Add optimize to day 13 (speeds up and makes it actually solve)

--- src/main/kotlin/sschr15/aocsolutions/Day13.kt
+++ src/main/kotlin/sschr15/aocsolutions/Day13.kt
@@ -1,13 +1,14 @@
 package sschr15.aocsolutions
 
 import com.sschr15.z3kt.int
-import com.sschr15.z3kt.solve
+import com.sschr15.z3kt.minimize
+import com.sschr15.z3kt.optimize
 import com.sschr15.z3kt.z3
 import sschr15.aocsolutions.util.*
 
 /**
  * AOC 2024 [Day 13](https://adventofcode.com/2024/day/13)
- * Challenge: let's win some prizes! it's going to take *a lot* of tokens
+ * Solves the challenge involving calculating tokens required for given prize targets.
  */
 object Day13 : Challenge {
     override fun solve() = challenge(2024, 13) {
@@ -21,8 +22,9 @@ object Day13 : Challenge {
             val regex =
                 """Button A: X([+-]\d+), Y([+-]\d+)\nButton B: X([+-]\d+), Y([+-]\d+)\nPrize: X=(\d+), Y=(\d+)""".toRegex()
 
-            prizeList = inputLines.map {
-                val match = regex.matchEntire(it) ?: error("Unexpected")
+            prizeList = inputLines.filter { it.isNotBlank() }.map {
+                val match = regex.matchEntire(it)
+                    ?: error("Input format does not match expected pattern")
                 match.groupValues.drop(1).map(String::toInt).toIntArray()
             }
 
@@ -30,12 +32,13 @@ object Day13 : Challenge {
                 prizeList.sumOf { (ax, ay, bx, by, px, py) ->
                     val aPresses by int
                     val bPresses by int
-
-                    val result = solve {
-                        add(aPresses gte 0)
-                        add(bPresses gte 0)
-                        add(ax * aPresses + bx * bPresses eq px)
-                        add(ay * aPresses + by * bPresses eq py)
+                
+                    val result = optimize {
+                        Add(aPresses gte 0)
+                        Add(bPresses gte 0)
+                        Add(ax * aPresses + bx * bPresses eq px)
+                        Add(ay * aPresses + by * bPresses eq py)
+                        minimize(aPresses + bPresses)
                     }
                     if (result == null) return@sumOf 0
 
@@ -49,11 +52,12 @@ object Day13 : Challenge {
                     val aPresses by int
                     val bPresses by int
 
-                    val result = solve {
-                        add(aPresses gte 0)
-                        add(bPresses gte 0)
-                        add(ax * aPresses + bx * bPresses eq (px + 10000000000000))
-                        add(ay * aPresses + by * bPresses eq (py + 10000000000000))
+                    val result = optimize {
+                        Add(aPresses gte 0)
+                        Add(bPresses gte 0)
+                        Add(ax * aPresses + bx * bPresses eq (px + 10000000000000))
+                        Add(ay * aPresses + by * bPresses eq (py + 10000000000000))
+                        minimize(aPresses + bPresses)
                     }
                     if (result == null) return@sumOf 0
 
