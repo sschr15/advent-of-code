b8dbf87e2f9a2babe7f78369ddc1d3fb5f9767e7
sschr15 <steven.sschr15@hotmail.com>
2024-12-25 00:46:28 -0600

    Create speedy d25 solution

--- src/main/kotlin/sschr15/aocsolutions/Day25.kt
+++ src/main/kotlin/sschr15/aocsolutions/Day25.kt
@@ -10,41 +10,63 @@ object Day25 : Challenge {
     override fun solve() = challenge(2024, 25) {
 //        test()
 
-        data class Thing(val a: Int, val b: Int, val c: Int, val d: Int, val e: Int)
+        data class Item(val a: Int, val b: Int, val c: Int, val d: Int, val e: Int) {
+            fun lockFitsKey(key: Item): Boolean =
+                a + key.a < 6 && b + key.b < 6 && c + key.c < 6 && d + key.d < 6 && e + key.e < 6
+        }
 
         splitBy("\n\n")
 
         part1 {
-            val locks = mutableListOf<IntArray>()
-            val keys = mutableListOf<IntArray>()
-            
+            val locks = mutableListOf<Item>()
+            val keys = mutableListOf<Item>()
+
             for (possibility in inputLines) {
-                val grid = possibility.lines().toGrid()
-                val list = IntArray(5)
-                val isLock = grid.first().all { it == '#' }
-                val toParse = if (!isLock) {
-                    grid.reversed()
-                } else grid
+                // Initial solution
+//                val grid = possibility.lines().toGrid()
+//                val list = IntArray(5)
+//                val isLock = grid.first().all { it == '#' }
+//                val toParse = if (!isLock) {
+//                    grid.reversed()
+//                } else grid
+//
+//                val transposed = toParse.transpose()
+//
+//                for (i in list.indices) {
+//                    list[i] = transposed[i].count { it == '#' } - 1
+//                }
+//
+//                val item = Item(list[0], list[1], list[2], list[3], list[4])
+//
+//                if (isLock) locks.add(item)
+//                else keys.add(item)
 
-                val transposed = toParse.transpose()
+                // Modified solution designed primarily for speed
+                val isLock = possibility.startsWith("#####")
+                var a = 0
+                var b = 0
+                var c = 0
+                var d = 0
+                var e = 0
 
-                for (i in list.indices) {
-                    list[i] = transposed[i].count { it == '#' } - 1
+                for (line in possibility.lines()) {
+                    if (line == "#####") continue
+                    if (line[0] == '#') a++
+                    if (line[1] == '#') b++
+                    if (line[2] == '#') c++
+                    if (line[3] == '#') d++
+                    if (line[4] == '#') e++
                 }
 
-                if (isLock) locks.add(list)
-                else keys.add(list)
+                val item = Item(a, b, c, d, e)
+                if (isLock) locks.add(item)
+                else keys.add(item)
             }
 
             var sum = 0
 
             for (lock in locks) for (key in keys) {
-                if (lock.toList().allIndexed { i, pos -> key[i] + pos < 6 }) {
-//                    println("success: ${lock.s} ${key.s}")
-                    sum++
-                } else {
-//                    println("failure: ${lock.s} ${key.s}")
-                }
+                if (lock.lockFitsKey(key)) sum++
             }
 
             sum
@@ -53,6 +75,4 @@ object Day25 : Challenge {
 
     @JvmStatic
     fun main(args: Array<String>) = println(solve())
-
-    val IntArray.s get() = joinToString(",")
 }
